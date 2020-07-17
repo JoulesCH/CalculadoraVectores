4249a31cba0a1a26c00851802517ced326cdd9a7
Julio Caesar <juliocaesar@unknown-3.local>
2020-07-17 13:36:24 -0500

    Incorporando abstraccion, herencia, encapsulamiento, modulacion y polimorfismo

---
+++ ModuloVectores.py
@@ -0,0 +1,99 @@
+class Vector:
+	def __init__(self,values):
+		"""
+		Recibe values(string) que contiene al vector con sus componentes separados por comas.
+		"""
+		self.vector = list(map(float, values.split(','))) 
+		self.__dimension = len(self.vector) #Abstracción: Encapsulamiento.
+
+
+	def sumar(self,vector):
+		"""
+		Recibe vector(objeto) para sumar. Regresa la suma de los dos vectores (list)
+		"""	
+		list=[]
+		if self.__dimension == vector.__dimension:
+			for i in range(self.__dimension):
+				list.append(self.vector[i]+vector.vector[i])
+
+			return list
+
+		else:
+			return 'No es posible sumar esos vetores'
+
+	def restar(self,vector):
+		"""
+		Recibe otro vector(objeto) para restar.
+		"""		
+		list=[]
+		if self.__dimension == vector.__dimension:
+			for i in range(self.__dimension):
+				list.append(self.vector[i] - vector.vector[i])
+
+			return list
+
+		else:
+			return '    No es posible restar esos vetores'
+
+	def multiplicar(self,escalar):
+		"""
+		Recibe un escalar(float) para multiplicar.
+		"""		
+		try:
+			escalar = float(escalar)
+		except:
+			return '    El escalar ingresado no es válido'
+		else:
+			return [x*escalar for x in self.vector ] #Se utlizó lis comprohension
+
+	def resultante(self,vector):
+		"""
+		Recibe otro vector(objeto) para calcular la resultante entre ellos.
+		"""	
+		if self.__dimension == vector.__dimension:
+			modulo1 = self.modulo()
+			modulo2 = vector.modulo()
+			angulo_radianes = math.radians(self.angulo(vector))
+
+			return pow(modulo1**2 + modulo2**2 + 2*modulo1*modulo2*(math.cos(angulo_radianes)),0.5)
+		else:
+			return '    No es posible calcular el angulo entre vetores'	
+		
+
+	def angulo(self,vector):
+		"""
+		Recibe otro vector(objeto) para calcular el angulo entre ellos.
+		"""	
+		if self.__dimension == vector.__dimension:
+			numerador = self.producto_punto(vector)	
+			denominador = self.producto_modulos(vector)
+
+			return math.degrees(math.acos(numerador/denominador))
+
+		else:
+			return '    No es posible calcular el angulo entre vetores'	
+		
+	def producto_punto(self,vector):
+		"""
+		Recibe otro vector (objeto) y regresa su producto punto
+		"""
+		sumatoria = 0
+		for i in range(self.dimension):
+			sumatoria += self.vector[i]*vector.vector[i]
+		return sumatoria	
+
+	def producto_modulos(self,vector):
+		"""
+		Recibe otro vector (objeto) y regresa el producto de sus modulos.
+		"""
+		return self.modulo()*vector.modulo()
+
+	def modulo(self):
+		"""
+		Regresa el modulo del vector propio.
+		"""
+		sumatoria = 0
+		for i in range(self.dimension):
+			sumatoria += self.vector[i]**2
+
+		return pow(sumatoria,0.5)			
--- main.py
+++ main.py
@@ -1,124 +1,57 @@
 #UTF-8
 import math
-
-class Vector:
-	def __init__(self,values):
-		"""
-		Recibe values(string) que contiene al vector con sus componentes separados por comas.
-		"""
-		self.vector = list(map(float, values.split(','))) 
-		self.dimension = len(self.vector)
-
-
-	def sumar(self,vector):
-		"""
-		Recibe vector(objeto) para sumar. Regresa la suma de los dos vectores (list)
-		"""	
-		list=[]
-		if self.dimension == vector.dimension:
-			for i in range(self.dimension):
-				list.append(self.vector[i]+vector.vector[i])
-
-			return list
-
-		else:
-			return 'No es posible sumar esos vetores'
-
-	def restar(self,vector):
-		"""
-		Recibe otro vector(objeto) para restar.
-		"""		
-		list=[]
-		if self.dimension == vector.dimension:
-			for i in range(self.dimension):
-				list.append(self.vector[i] - vector.vector[i])
-
-			return list
-
-		else:
-			return '    No es posible restar esos vetores'
-
-	def multiplicar(self,escalar):
-		"""
-		Recibe un escalar(float) para multiplicar.
-		"""		
-		try:
-			escalar = float(escalar)
-		except:
-			return '    El escalar ingresado no es válido'
-		else:
-			return [x*escalar for x in self.vector ] #Se utlizó lis comprohension
-
-	def resultante(self,vector):
-		"""
-		Recibe otro vector(objeto) para calcular la resultante entre ellos.
-		"""	
-		if self.dimension == vector.dimension:
-			modulo1 = self.modulo()
-			modulo2 = vector.modulo()
-			angulo_radianes = math.radians(self.angulo(vector))
-
-			return pow(modulo1**2 + modulo2**2 + 2*modulo1*modulo2*(math.cos(angulo_radianes)),0.5)
-		else:
-			return '    No es posible calcular el angulo entre vetores'	
-		
-
-	def angulo(self,vector):
-		"""
-		Recibe otro vector(objeto) para calcular el angulo entre ellos.
-		"""	
-		if self.dimension == vector.dimension:
-			numerador = self.producto_punto(vector)	
-			denominador = self.producto_modulos(vector)
-
-			return math.degrees(math.acos(numerador/denominador))
-
-		else:
-			return '    No es posible calcular el angulo entre vetores'	
-		
-	def producto_punto(self,vector):
-		"""
-		Recibe otro vector (objeto) y regresa su producto punto
-		"""
-		sumatoria = 0
-		for i in range(self.dimension):
-			sumatoria += self.vector[i]*vector.vector[i]
-		return sumatoria	
-
-	def producto_modulos(self,vector):
-		"""
-		Recibe otro vector (objeto) y regresa el producto de sus modulos.
-		"""
-		return self.modulo()*vector.modulo()
-
-	def modulo(self):
-		"""
-		Regresa el modulo del vector propio.
-		"""
-		sumatoria = 0
-		for i in range(self.dimension):
-			sumatoria += self.vector[i]**2
-
-		return pow(sumatoria,0.5)						
+from ModuloVectores import Vector #Modulacion			
 
 		
+class Menu_princial:
+	def __init__(self,opcion='0'):
+		self.opcion = opcion
 
-def menu_principal():	
-	print('-----------------*-------------')
-	print('     Calculadora de vectores')
-	print('\n \n        [1]Sumar vectores')
-	print('        [2]Restar vectores')
-	print('        [3]Multiplicar vector por escalar')
-	print('        [4]Calcular resultante entre vectores')
-	print('        [5]Calcular angulo entre dos vectores')
-	return input('\n            => ')			
+	def imprimir(slef):
+		print('-----------------*-------------')
+		print('     Calculadora de vectores')
+		print('\n \n        [1]Sumar vectores')
+		print('        [2]Restar vectores')
+		print('        [3]Multiplicar vector por escalar')
+		print('        [4]Calcular resultante entre vectores')
+		print('        [5]Calcular angulo entre dos vectores')
 
+		self.opcion = input('\n            => ')
+
+class Maestro (Menu_princial): #Herencia
+	def __init__(self):
+		Menu_princial.__init__()
+
+	def pedir_datos(self):
+		if self.opcion == '1' or self.opcion =='2' or self.opcion =='4' or or self.opcion =='5':
+			try:
+				self.vector1 = Vector(input(texto))
+				self.vector2 = Vector(input(texto)) 
+			except:
+				print('    Los vectores unicamente se componen de números')	
+				return 0
+			else:	
+				return 1
+		
+		elif self.opcion =='3':
+			try:
+				self.vector1 = Vector(input(texto))
+			except:
+				print('    Los vectores unicamente se componen de números')	
+				return 0
+			
+			else:
+				self.escalar = input('    Ingresa escalar =>')
+				return 1
+
+			
 if __name__ == '__main__':
 
 	texto = '    Ingresa vector separado por comas => '
 
 	while True:
-		opcion = menu_principal()
+		menu = Menu_princial()
+		opcion = menu.imprimir()
 		if opcion == '1': # Suma
 			print('\n\n-----------------*------------------')
 			print('     [1]Sumar vectores\n')
